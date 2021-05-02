dc5c9844451f2fdca633dd57e763f2604d760591
asmazeeshan <80879655+asmazeeshan@users.noreply.github.com>
2021-05-02 21:00:11 +0500

    add all

---
+++ wp-content/themes/Divichild/functions.php
@@ -0,0 +1,20 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    $parenthandle = 'parent-style';
+    $theme = wp_get_theme();
+    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+        array(), 
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),array( $parenthandle ),
+        $theme->get('Version') 
+    );
+    wp_enqueue_script( $parenthandle , get_stylesheet_directory_uri() . '/js/script.js',
+     array());
+     wp_enqueue_script( 'child-script', get_stylesheet_uri(),array( $parenthandle ),
+     $theme->get('Version') 
+ );
+}
+
+?>
